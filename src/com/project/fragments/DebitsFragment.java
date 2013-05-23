74bd55bdc1f84640fef87d0947c8cc5a09df8b13
noorazmi <noorazmi@rediff.com>
2013-05-23 23:38:27 +0530

    fres files added

--- src/com/project/fragments/DebitsFragment.java
+++ src/com/project/fragments/DebitsFragment.java
@@ -12,6 +12,7 @@ public class DebitsFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+	
         return inflater.inflate(R.layout.fragment_debits, container, false);
     }
 }
